1cc526815207ca0e6d9cb91c495cdb8f43ff5aa8
Jacob Pell <pelljacoba@gmail.com>
2025-08-22 17:44:22 -0400

    Initial commit with dependencies. Also first steps in nav bar.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Sylphaxiom.com Website
 
 This site will serve as my primary website for personal and professional needs.
+
+## Installed Components
+
+- @mui/material
+- @emotion/react
+- @emotion/styled
+- playwright/test
+- @mui/icons-material
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <React.Fragment>
+      <CssBaseline />
+      <App />
+    </React.Fragment>
+  </React.StrictMode>
+);
