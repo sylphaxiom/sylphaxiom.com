846fb61ae44032f25333f37164c18bd451a91374
Jacob Pell <pelljacoba@gmail.com>
2025-08-22 23:36:43 -0400

    Accidentally worked too long without a commit. Built navbar, working on style choices.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-//import * as React from 'react'
+import * as React from "react";
 import Box from "@mui/material/Box";
 import "./App.css";
 import Navigation from "./components/navigation";
 
 export default function App() {
+  const [page, setPage] = React.useState("home");
+  const handleSelect = (pg: string) => {
+    setPage(pg);
+  };
+
   return (
-    <Box sx={{ maxWidth: "80%" }}>
-      <Navigation />
+    <Box sx={{ minWidth: 1, mx: "auto", padding: 0 }}>
+      <Navigation current={page} onChange={handleSelect} />
     </Box>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,101 @@
 import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Link from "@mui/material/Link";
 
-export default function Navigation() {
-  return <div>navigation</div>;
+interface Props {
+  current: string;
+  onChange: (pg: string) => void;
+}
+
+function firstUpper(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export default function Navigation({ current, onChange }: Props) {
+  const pages = ["person", "stuff", "things", "contact"];
+  let title: string;
+  switch (current) {
+    case "person":
+      title = "This is the Guy...";
+      break;
+
+    case "stuff":
+      title = "Oh look! Nerdy Stuff...";
+      break;
+
+    case "things":
+      title = "...More Nerdy Things...";
+      break;
+
+    case "contact":
+      title = "Talk Nerdy To Me!";
+      break;
+
+    default:
+      title = "Sylphaxiom Creative";
+  }
+
+  if (!pages.includes(current)) {
+    current = "person";
+  }
+
+  return (
+    <Grid
+      container
+      sx={{
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: 150,
+        position: "fixed",
+        width: "100%",
+        top: 0,
+      }}
+    >
+      <Grid size={1} sx={{ float: "left" }}>
+        <Link>
+          <img
+            src={"/sylphaxiom_web.svg"}
+            alt="curious guy in a browser"
+            width={100}
+            height={100}
+          />
+        </Link>
+      </Grid>
+      <Grid size={"grow"}>
+        <Typography variant={"h2"} component={"h1"} noWrap color={"primary"}>
+          {title}
+        </Typography>
+      </Grid>
+      <Grid size={3} sx={{ float: "right" }}>
+        <Tabs
+          aria-label="nav tabs"
+          role="navigation"
+          id="navTabRoot"
+          indicatorColor="secondary"
+          onChange={(event: React.SyntheticEvent, nxtPg) => {
+            event.preventDefault;
+            onChange(nxtPg);
+          }}
+          value={current}
+          centered
+        >
+          {pages.map((page, index) => (
+            <Tab
+              LinkComponent={"a"}
+              label={firstUpper(page)}
+              value={page}
+              aria-controls={page}
+              key={"tab" + index}
+              id={"tab" + index}
+              href={"#" + page}
+              sx={{}}
+            />
+          ))}
+        </Tabs>
+      </Grid>
+    </Grid>
+  );
 }
