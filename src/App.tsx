87a11ab5ecab9748473cd4463d4f32928b9423cb
Jacob Pell <pelljacoba@gmail.com>
2025-08-25 10:05:45 -0400

    Placed generic container on main page.

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,43 @@ import Box from "@mui/material/Box";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import Container from "@mui/material/Container";
+import Home from "./components/Home";
+import Person from "./components/Person";
+import Stuff from "./components/Stuff";
+import Things from "./components/Things";
+import Contact from "./components/Contact";
 
 export default function App() {
   const [page, setPage] = React.useState("home");
   const handleSelect = (pg: string) => {
     setPage(pg);
   };
+  let children: React.ReactNode = "";
+  switch (page) {
+    case "home":
+      children = <Home />;
+      break;
+    case "person":
+      children = <Person />;
+      break;
+    case "stuff":
+      children = <Stuff />;
+      break;
+    case "things":
+      children = <Things />;
+      break;
+    case "contact":
+      children = <Contact />;
+      break;
+    default:
+      children = <Home />;
+      break;
+  }
 
   return (
     <Box sx={{ minWidth: 1, mx: "auto", padding: 0 }}>
       <Navigation current={page} onChange={handleSelect} />
-      <Container maxWidth="lg"></Container>
+      <Container maxWidth="xl">{children}</Container>
     </Box>
   );
 }
