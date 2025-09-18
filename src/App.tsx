3f3b255b02eb413eab452149018c63a7dfdd9a5f
Jacob Pell <pelljacoba@gmail.com>
2025-09-18 01:12:20 -0400

    warnings were errors during build

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Box from "@mui/material/Box";
 import Navigation from "./components/Navigation";
 import Container from "@mui/material/Container";
 import Home from "./components/Home";
-import Stuff from "./components/Stuff";
-import Things from "./components/Things";
-import Contact from "./components/Contact";
+// import Stuff from "./components/Stuff";
+// import Things from "./components/Things";
+// import Contact from "./components/Contact";
 import Cover from "./components/Cover";
 import Portfolio from "./components/Portfolio";
 import { useCookies } from "react-cookie";
@@ -25,27 +25,27 @@ export default function App() {
     }
   }
 
-  let children: React.ReactNode = "";
-  switch (page) {
-    case "home":
-      children = <Home />;
-      break;
-    case "portfolio":
-      children = <Portfolio />;
-      break;
-    case "stuff":
-      children = <Stuff />;
-      break;
-    case "things":
-      children = <Things />;
-      break;
-    case "contact":
-      children = <Contact />;
-      break;
-    default:
-      children = <Home />;
-      break;
-  }
+  // let children: React.ReactNode = "";
+  // switch (page) {
+  //   case "home":
+  //     children = <Home />;
+  //     break;
+  //   case "portfolio":
+  //     children = <Portfolio />;
+  //     break;
+  //   case "stuff":
+  //     children = <Stuff />;
+  //     break;
+  //   case "things":
+  //     children = <Things />;
+  //     break;
+  //   case "contact":
+  //     children = <Contact />;
+  //     break;
+  //   default:
+  //     children = <Home />;
+  //     break;
+  // }
   return (
     <Box id="everything" sx={{ minWidth: 1, mx: "auto", p: 0 }}>
       <Navigation current={page} />
