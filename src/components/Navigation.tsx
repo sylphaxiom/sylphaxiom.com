b444ec32d146b99a6478bd115a55313077d64486
Jacob Pell <pelljacoba@gmail.com>
2025-09-19 17:59:29 -0400

    Fully adjusted. Moving to web for live testing.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
-// import * as React from "react";
+import * as React from "react";
 import * as motion from "motion/react-client";
+import * as motions from "motion/react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Tabs from "@mui/material/Tabs";
@@ -11,26 +12,27 @@ import { stagger } from "motion";
 import { Link, Outlet } from "react-router";
 import { useMatch } from "react-router";
 
-// function firstUpper(str: string) {
-//   return str.charAt(0).toUpperCase() + str.slice(1);
-// }
-
 export default function Navigation() {
   const creative = ["home", "people", "projects", "contact"];
-  const portfolio = ["port", "web", "assets", "writing", "contact"];
+  const portfolio = ["portfolio", "web", "assets", "writing", "contact"];
   const matchCreative = useMatch("/creative/*");
   const matchPortfolio = useMatch("/portfolio/*");
   let pages: string[];
   let base: string;
+  let group: string;
   if (matchCreative) {
     pages = creative;
-    base = matchCreative.pathnameBase;
+    group = "creative";
+    base = matchCreative.pathname.split("/")[2] || "home";
   } else if (matchPortfolio) {
     pages = portfolio;
-    base = matchPortfolio.pathnameBase;
+    group = "portfolio";
+    base = matchPortfolio.pathname.split("/")[2] || "portfolio";
   } else {
+    base = "";
     throw new Error("No match for current route");
   }
+  const [current, setCurrent] = React.useState(base);
   const disabled: string[] = [];
   //   "people",
   //   "projects",
@@ -50,7 +52,7 @@ export default function Navigation() {
       title = "Our Creative Team";
       break;
 
-    case "proj":
+    case "projects":
       title = "Our Projects";
       break;
 
@@ -58,7 +60,7 @@ export default function Navigation() {
       title = "Let's Create!";
       break;
 
-    case "port":
+    case "portfolio":
       title = "Creator Portfolio";
       break;
 
@@ -70,6 +72,10 @@ export default function Navigation() {
       title = "Digital Art and Assets";
       break;
 
+    case "writing":
+      title = "Writing and Storytelling";
+      break;
+
     default:
       !base && base === "/";
       title = "Sylphaxiom Creative";
@@ -77,10 +83,17 @@ export default function Navigation() {
 
   // Title animation
 
+  React.useEffect(() => {
+    setCurrent(base);
+  }, [base]);
+
   const variants = {
-    start: { y: -100, Opacity: 0 },
+    start: { y: -100, opacity: 0 },
     animate: { y: 0, opacity: 1 },
+    // exit: { y: 20, opacity: 0 },
   };
+
+  console.log("base is", base, "pages is", pages, "group is", group);
 
   return (
     <Box id="everything" sx={{ minWidth: 1, mx: "auto", p: 0 }}>
@@ -98,11 +111,12 @@ export default function Navigation() {
         }}
       >
         <Grid size={1} sx={{ float: "left" }}>
-          <Button href="home">
+          <Button component={Link} to={"/"}>
             <motion.div
               initial={{ scale: 0 }}
               animate={{ scale: 1, rotate: 1080 }}
               transition={{ duration: 0.8 }}
+              className={group}
             >
               <img
                 src={"/sylphaxiom_web_512x.svg"}
@@ -121,37 +135,41 @@ export default function Navigation() {
             noWrap
             color={"primary"}
           >
-            <motion.div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                overflow: "visible",
-              }}
-              transition={{ delayChildren: stagger(0.05) }}
-              initial="start"
-              animate="animate"
-              variants={variants}
-            >
-              {title.split("").map((char: string, index: number) => {
-                if (char === " ") {
-                  char = "\u00A0";
-                }
-                return (
-                  <motion.span
-                    key={char + index}
-                    variants={variants}
-                    transition={{
-                      type: "spring",
-                      bounce: 0.5,
-                      duration: 0.5,
-                    }}
-                  >
-                    {char}
-                  </motion.span>
-                );
-              })}
-            </motion.div>
+            <motions.AnimatePresence mode="wait">
+              <motion.div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  overflow: "visible",
+                }}
+                key={current}
+                transition={{ delayChildren: stagger(0.03) }}
+                initial="start"
+                animate="animate"
+                // exit="exit"
+                variants={variants}
+              >
+                {title.split("").map((char: string, index: number) => {
+                  if (char === " ") {
+                    char = "\u00A0";
+                  }
+                  return (
+                    <motion.span
+                      key={char + index}
+                      variants={variants}
+                      transition={{
+                        type: "spring",
+                        bounce: 0.5,
+                        duration: 0.5,
+                      }}
+                    >
+                      {char}
+                    </motion.span>
+                  );
+                })}
+              </motion.div>
+            </motions.AnimatePresence>
           </Typography>
         </Grid>
         <Grid size={3} sx={{ float: "right" }}>
@@ -166,7 +184,7 @@ export default function Navigation() {
             {pages.map((page, index) => (
               <Tab
                 component={Link}
-                to={page}
+                to={page === "home" ? "/creative" : page}
                 label={page}
                 value={page}
                 disabled={disabled.includes(page) || false}
