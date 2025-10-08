c86d9cb2db92803b75e3386300091954812768b7
Jacob Pell <pelljacoba@gmail.com>
2025-10-07 22:33:38 -0400

    Corrected API issues and finish API testing.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Radio from "@mui/material/Radio";
@@ -10,40 +11,142 @@ import FormLabel from "@mui/material/FormLabel";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormGroup from "@mui/material/FormGroup";
 import Checkbox from "@mui/material/Checkbox";
-// import Card from "@mui/material/Card";
 import Divider from "@mui/material/Divider";
 import TextField from "@mui/material/TextField";
-import TextAreaAutosize from "@mui/material/TextareaAutosize";
+import Textarea from "@mui/joy/Textarea";
+import { useFetcher } from "react-router";
+import type { Route } from "./+types/Contact";
+
+export async function clientAction({ request }: Route.ClientLoaderArgs) {
+  await new Promise((res) => setTimeout(res, 1000));
+  let data = await request.formData();
+  console.log("You're in the action function");
+  const name = String(data.get("name"));
+  const who = String(data.get("who_group"));
+  const email = String(data.get("email"));
+  let subject = String(data.get("subject")); // needs to be mutable since it may not be set by default.
+  const message = String(data.get("message"));
+  let recipient = "";
+  let creator = "";
+  let errors = { name: "", email: "", who: "", subject: "", message: "" };
+  let err = false;
+
+  // Validate name field, if needed
+  if (Boolean(name.match(/^[A-Za-z\s]+$/))) {
+    console.log("looks good: " + name);
+  } else {
+    errors.name =
+      "Looks like theres some characters other than letters and spaces, have another look and try again: " +
+      name;
+    err = true;
+  }
+
+  // Validate email field
+  let validator = email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  if (Boolean(validator)) {
+    console.log("looks good: " + email);
+  } else {
+    errors.email =
+      "There appears to be an issue with your email address, have another look and try again: " +
+      email;
+    err = true;
+  }
+
+  // Apply Who logic
+  switch (who) {
+    case "web":
+      recipient = "webmaster@sylphaxiom.com";
+      break;
+    case "creator":
+      recipient = creator + "@sylphaxiom.com";
+      subject =
+        "You have a new direct message from the sylphaxiom.com web form"; // setting subject since it shouldn't be populated
+      break;
+    case "intake":
+      recipient = "intake@sylphaxiom.com";
+      subject = "A new client request has arrived"; // setting subject since it shouldn't be populated
+      break;
+  }
+
+  // Subject validation
+  if (subject.length < 255) {
+    console.log("looks good: " + subject);
+  } else {
+    err = true;
+    errors.subject =
+      "Your subject is a bit too long. Please keep it under 255 characters.";
+  }
+
+  // Message validation
+  if (message.length < 65535) {
+    console.log("looks good: " + subject);
+  } else {
+    err = true;
+    errors.subject =
+      "Your message is a bit too long. Please keep it under (only) 65,535 characters.";
+  }
+
+  // Form the API call and await the response
+  const jsonStr = JSON.stringify({
+    name: name,
+    subject: subject,
+    who: who,
+    email: email,
+    message: message,
+    recipient: recipient,
+  });
+  console.log(jsonStr);
+}
 
 export default function Contact() {
+  let fetcher = useFetcher();
+  const [text, setText] = React.useState("");
   const [radioHelper, setRadioHelper] = React.useState(
     "Inquire about having a website, asset, or anything else you want created."
   );
-  const [nameHelper, setNameHelper] = React.useState("");
-  const [creator, setCreator] = React.useState(false);
+  const [who, setWho] = React.useState(fetcher.data?.who_group || "intake");
+  const [creator, setCreator] = React.useState("jacob");
+  let nameHelper = "";
+  let emailHelper = "";
 
-  console.log(setNameHelper);
-
-  const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget.value;
     switch (target) {
       case "web":
         setRadioHelper(
           "Contact the Webmaster with any issues or inquiries relating to this website"
         );
-        setCreator(false);
+        setWho(target);
         break;
       case "intake":
         setRadioHelper(
           "Inquire about having a website, asset, or anything else you want created."
         );
-        setCreator(false);
+        setWho(target);
         break;
       case "creator":
         setRadioHelper(
           "Reach out to a creator directly... for whatever reason, I guess."
         );
-        setCreator(true);
+        setWho(target);
+        break;
+      case "jacob":
+        setCreator("jacob");
+        break;
+      case "ty":
+        setCreator("ty");
+        break;
+      default:
+        console.log(
+          "What kind of fuckery did you do to get here? Only checkbox and radio values should be passed in here.\nLooks like your dumb-ass forgot to add a case. Tsk, tsk."
+        );
+        console.log(
+          "ugh, you complain too much. Here is some data:\n" + target
+        );
     }
   };
 
@@ -63,101 +166,159 @@ export default function Contact() {
           <Typography variant="h3">
             Fill out the form below and we'll get right back to you!
           </Typography>
-          <FormControl sx={{ m: 4 }} focused>
-            <FormLabel id="who_label" sx={{ mt: 2 }}>
-              Who do you want to contact?
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="who_label"
-              name="who_group"
-              sx={{ justifyContent: "space-around" }}
-              onChange={handleRadioChange}
-              defaultValue={"intake"}
-            >
-              <FormControlLabel
-                value={"web"}
-                control={<Radio />}
-                label={"Webmaster"}
-              />
-              <FormControlLabel
-                value={"intake"}
-                control={<Radio />}
-                label={"Services Inquiry"}
-              />
-              <FormControlLabel
-                value={"creator"}
-                control={<Radio />}
-                label={"Creator"}
-              />
-            </RadioGroup>
-            <FormHelperText id="radioHelper_field" sx={{ textAlign: "center" }}>
-              {radioHelper}
-            </FormHelperText>
-            <Divider sx={{ m: 2 }} />
-            {creator && (
-              <>
-                <FormLabel component={"legend"}>Creators</FormLabel>
-                <FormGroup row sx={{ justifyContent: "space-around" }}>
-                  <FormControlLabel
-                    control={<Checkbox checked={false} name="jacob" />}
-                    label="Jacob"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox checked={false} name="ty" />}
-                    label="Ty"
-                  />
-                </FormGroup>
-                <Divider sx={{ m: 2 }} />
-              </>
-            )}
-            <FormLabel id="subject_label">What's on your mind?</FormLabel>
-            <Grid container>
-              <Grid size={6}>
-                <TextField
-                  id="name"
-                  label="Name"
-                  variant="standard"
-                  sx={{ width: 0.8 }}
+          <fetcher.Form method="POST">
+            <FormControl sx={{ m: 4 }} focused>
+              <FormLabel id="who_label" sx={{ mt: 2 }}>
+                Who do you want to contact?
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="who_label"
+                name="who_group"
+                sx={{ justifyContent: "space-around" }}
+                onChange={handleChange}
+                defaultValue={"intake"}
+              >
+                <FormControlLabel
+                  value={"web"}
+                  control={<Radio />}
+                  label={"Webmaster"}
                 />
-                <FormHelperText
-                  id="nameHelper_field"
-                  sx={{ mb: 2, textAlign: "center" }}
-                >
-                  {nameHelper}
-                </FormHelperText>
-              </Grid>
-              <Grid size={6}>
-                <TextField
-                  id="email"
-                  label="Email"
-                  variant="standard"
-                  sx={{ width: 0.8 }}
+                <FormControlLabel
+                  value={"intake"}
+                  control={<Radio />}
+                  label={"Services Inquiry"}
                 />
-                <FormHelperText
-                  id="nameHelper_field"
-                  sx={{ mb: 2, textAlign: "center" }}
+                <FormControlLabel
+                  value={"creator"}
+                  control={<Radio />}
+                  label={"Creator"}
+                />
+              </RadioGroup>
+              <FormHelperText
+                id="radioHelper_field"
+                sx={{ textAlign: "center" }}
+              >
+                {radioHelper}
+              </FormHelperText>
+              <Divider sx={{ m: 2 }} />
+              {who === "creator" && (
+                <>
+                  <FormLabel component={"legend"}>Creators</FormLabel>
+                  <FormGroup row sx={{ justifyContent: "space-around" }}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={Boolean(creator.match("jacob")) || false}
+                          value="jacob"
+                          onChange={handleChange}
+                          name="jacob"
+                        />
+                      }
+                      label="Jacob"
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={Boolean(creator.match("ty")) || false}
+                          value="ty"
+                          onChange={handleChange}
+                          name="ty"
+                        />
+                      }
+                      label="Ty"
+                    />
+                  </FormGroup>
+                  <Divider sx={{ m: 2 }} />
+                </>
+              )}
+              {who === "web" ?
+                <Grid
+                  container
+                  spacing={2}
+                  sx={{ alignItems: "center", textAlign: "left" }}
                 >
-                  {nameHelper}
-                </FormHelperText>
+                  <Grid size={3}>
+                    <FormLabel id="subject_label">What's up?</FormLabel>
+                  </Grid>
+                  <Grid size={9}>
+                    <TextField
+                      id="name"
+                      label="Subject"
+                      variant="standard"
+                      error={fetcher.data?.error}
+                      sx={{ width: 1 }}
+                    />
+                    <FormHelperText
+                      id="nameHelper_field"
+                      sx={{ mb: 2, textAlign: "center" }}
+                    >
+                      {nameHelper}
+                    </FormHelperText>
+                  </Grid>
+                </Grid>
+              : <FormLabel id="subject_label">
+                  Leave your info here and{" "}
+                  {who === "creator" ?
+                    creator.replace(creator[0], creator[0].toUpperCase())
+                  : "someone"}{" "}
+                  will get back to you as soon as they can!
+                </FormLabel>
+              }
+              <Grid container spacing={2}>
+                <Grid size={6}>
+                  <TextField
+                    id="name"
+                    label="Name"
+                    variant="standard"
+                    error={fetcher.data?.error}
+                    sx={{ width: 1 }}
+                  />
+                  <FormHelperText
+                    id="nameHelper_field"
+                    sx={{ mb: 2, textAlign: "center" }}
+                  >
+                    {nameHelper}
+                  </FormHelperText>
+                </Grid>
+                <Grid size={6}>
+                  <TextField
+                    id="email"
+                    label="Email"
+                    variant="standard"
+                    error={fetcher.data?.error}
+                    sx={{ width: 1 }}
+                  />
+                  <FormHelperText
+                    id="nameHelper_field"
+                    sx={{ mb: 2, textAlign: "center" }}
+                  >
+                    {emailHelper}
+                  </FormHelperText>
+                </Grid>
               </Grid>
-            </Grid>
-            <TextAreaAutosize
-              id="message"
-              minRows={4}
-              aria-label="message"
-              placeholder="Other words..."
-              style={{
-                margin: "1.8em",
-                marginTop: 0,
-                background: "whitesmoke",
-                borderColor: "rgb(0,0,0,0.3)",
-                borderRadius: 5,
-                fontSize: "1em",
-                padding: 10,
-              }}
-            />
-          </FormControl>
+              <Textarea
+                id="message"
+                minRows={4}
+                value={text}
+                onChange={(event) => setText(event.target.value)}
+                name="message"
+                aria-label="message"
+                variant="outlined"
+                endDecorator={
+                  <Typography sx={{ ml: "auto", opacity: 0.8 }}>
+                    {text.length} character(s)
+                  </Typography>
+                }
+                error={fetcher.data?.error}
+                sx={{ backgroundColor: "whitesmoke" }}
+                placeholder="Stuff... Things... Whatever..."
+              />
+            </FormControl>
+            <Button variant="text" type="submit">
+              Submit
+            </Button>
+          </fetcher.Form>
         </Stack>
       </Grid>
     </Grid>
