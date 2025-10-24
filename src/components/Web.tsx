e3614771c161c9c48fff330859c0d04249bf2daf
Jacob Pell <pelljacoba@gmail.com>
2025-10-24 19:13:09 -0400

    Not sure about the card

--- src/components/Web.tsx
+++ src/components/Web.tsx
@@ -1,5 +1,29 @@
 // import * as React from 'react'
+import ConstructionIcon from "@mui/icons-material/Construction";
+import { Card, CardContent, CardHeader, Divider, Grid } from "@mui/material";
 
 export default function Web() {
-  return <div id="web_content">The websites</div>;
+  return (
+    <Grid container spacing={1} id="web_content">
+      <Grid size={4}>
+        <Card>
+          <CardHeader
+            title="Functional"
+            subheader="Websites built for a purpose"
+          />
+          <Divider variant="middle" />
+          <CardContent>
+            Let's face it, everyone who wants a website has a stated purpose for
+            that site. So why not cater your site to meet your needs? There are
+            a million sites that will give you a "free" website or have some AI
+            tool that will make a site for you, but those always feel lacking,
+            without purpose. I know people have needs and I'm here to meet those
+            needs, in the form of a website or some other asset that will
+            enhance your life (and business) in some small way.
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid size={8}></Grid>
+    </Grid>
+  );
 }
