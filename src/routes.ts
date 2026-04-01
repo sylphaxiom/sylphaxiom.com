1805ddd8fe0e76d07581233e14b44f0ea63d8727
Jacob Pell <pelljacoba@gmail.com>
2026-04-01 17:55:18 -0400

    Error during build showed a warning I didn't heed.

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import {type RouteConfig, route, layout, index, prefix,} from "@react-router/dev/routes"
+import {type RouteConfig, route, layout, index} from "@react-router/dev/routes"
 import "react-router"
 declare module "react-router"{
     interface AppLoadContext {
